f4c057cd2b5f82e05f9b4ccab96bbecc40d1d8df
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-11 15:57:21 +0700

    update data provider test at CustomerDataSource::isAccountCompatibleDataProvider()

--- tests/Unit/Google/CustomerDataSourceTest.php
+++ tests/Unit/Google/CustomerDataSourceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Google;
 
 use App\Google\CustomerDataSource;
 use App\Objects\Contracts\AccountInterface;
+use JetBrains\PhpStorm\ArrayShape;
 use PHPUnit\Framework\TestCase;
 
 class CustomerDataSourceTest extends TestCase
@@ -27,16 +28,25 @@ class CustomerDataSourceTest extends TestCase
         self::assertInstanceOf(AccountInterface::class, self::$customerDataSource->find("foo"));
     }
 
+    #[ArrayShape([
+        'customers/123' => "array",
+        'customer/123' => "array",
+        '123/customers' => "array",
+        '123/customer' => "array",
+        'customer' => "array",
+        'string int' => "array",
+        'empty string' => "array"
+    ])]
     public function isAccountCompatibleDataProvider(): array
     {
         return [
-            [true, 'customers/123'],
-            [false, 'customer/123'],
-            [false, '123/customers'],
-            [false, '123/customer'],
-            [false, 'customer'],
-            [false, '123'],
-            [false, ''],
+            'customers/123' => [true, 'customers/123'],
+            'customer/123' => [false, 'customer/123'],
+            '123/customers' => [false, '123/customers'],
+            '123/customer' => [false, '123/customer'],
+            'customer' => [false, 'customer'],
+            'string int' => [false, '123'],
+            'empty string' => [false, ''],
         ];
     }
 
