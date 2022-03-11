4255ff9cf8a50dba35dfaa0a27d648d31b105757
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-11 15:57:50 +0700

    update data provider test at AdAccountDataSourceTest::isAccountCompatibleDataProvider()

--- tests/Unit/Facebook/AdAccountDataSourceTest.php
+++ tests/Unit/Facebook/AdAccountDataSourceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Facebook;
 
 use App\Facebook\AdAccountDataSource;
 use App\Objects\Contracts\AccountInterface;
+use JetBrains\PhpStorm\ArrayShape;
 use PHPUnit\Framework\TestCase;
 
 class AdAccountDataSourceTest extends TestCase
@@ -27,16 +28,25 @@ class AdAccountDataSourceTest extends TestCase
         self::assertInstanceOf(AccountInterface::class, self::$adAccountDataSource->find('foo'));
     }
 
+    #[ArrayShape([
+        'act_123' => "array",
+        'act123' => "array",
+        '123_act' => "array",
+        '123act' => "array",
+        'string int' => "array",
+        'act' => "array",
+        'empty string' => "array"
+    ])]
     public function isAccountCompatibleDataProvider(): array
     {
         return [
-            [true, 'act_123'],
-            [false, 'act123'],
-            [false, '123_act'],
-            [false, '123act'],
-            [false, '123'],
-            [false, 'act'],
-            [false, ''],
+            'act_123' => [true, 'act_123'],
+            'act123' => [false, 'act123'],
+            '123_act' => [false, '123_act'],
+            '123act' => [false, '123act'],
+            'string int' => [false, '123'],
+            'act' => [false, 'act'],
+            'empty string' => [false, ''],
         ];
     }
 
