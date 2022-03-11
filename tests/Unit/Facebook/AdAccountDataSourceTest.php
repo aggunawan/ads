b371ffe66c229f8026828102735121a4861f3e6b
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-11 15:11:34 +0700

    implement account id compatibility check at Facebook account data provider

--- tests/Unit/Facebook/AdAccountDataSourceTest.php
+++ tests/Unit/Facebook/AdAccountDataSourceTest.php
@@ -8,9 +8,43 @@ use PHPUnit\Framework\TestCase;
 
 class AdAccountDataSourceTest extends TestCase
 {
+    static public ?AdAccountDataSource $adAccountDataSource;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$adAccountDataSource = new AdAccountDataSource('bar');
+        parent::setUpBeforeClass();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$adAccountDataSource = null;
+        parent::tearDownAfterClass();
+    }
+
     public function testFind()
     {
-        $accountDataSource = new AdAccountDataSource('foo');
-        self::assertInstanceOf(AccountInterface::class, $accountDataSource->find('foo'));
+        self::assertInstanceOf(AccountInterface::class, self::$adAccountDataSource->find('foo'));
+    }
+
+    public function isAccountCompatibleDataProvider(): array
+    {
+        return [
+            [true, 'act_123'],
+            [false, 'act123'],
+            [false, '123_act'],
+            [false, '123act'],
+            [false, '123'],
+            [false, 'act'],
+            [false, ''],
+        ];
+    }
+
+    /**
+     * @dataProvider isAccountCompatibleDataProvider
+     */
+    public function testIsAccountCompatible(bool $condition, string $accountId)
+    {
+        self::assertSame($condition, self::$adAccountDataSource->isAccountCompatible($accountId));
     }
 }
