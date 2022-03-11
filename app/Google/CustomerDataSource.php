d02d4b9df2da6d4c3545beedde750215b95845a9
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-11 15:10:19 +0700

    implement account id compatibility check at Google account data provider

--- app/Google/CustomerDataSource.php
+++ app/Google/CustomerDataSource.php
@@ -5,6 +5,7 @@ namespace App\Google;
 use App\DataSource\Abstracts\BaseAccountDataSource;
 use App\Objects\Account;
 use App\Objects\Contracts\AccountInterface;
+use Illuminate\Support\Str;
 use JetBrains\PhpStorm\Pure;
 
 class CustomerDataSource extends BaseAccountDataSource
@@ -13,5 +14,11 @@ class CustomerDataSource extends BaseAccountDataSource
     public function find(string $accountId): ?AccountInterface
     {
         return new Account($accountId, 'Foo');
+    }
+
+    #[Pure]
+    public function isAccountCompatible(string $id): bool
+    {
+        return Str::startsWith($id, 'customers/');
     }
 }
--- tests/Unit/Google/CustomerDataSourceTest.php
+++ tests/Unit/Google/CustomerDataSourceTest.php
@@ -8,9 +8,43 @@ use PHPUnit\Framework\TestCase;
 
 class CustomerDataSourceTest extends TestCase
 {
+    static public ?CustomerDataSource $customerDataSource;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$customerDataSource = new CustomerDataSource('bar');
+        parent::setUpBeforeClass();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$customerDataSource = null;
+        parent::tearDownAfterClass();
+    }
+
     public function testFind()
     {
-        $customerDataSource = new CustomerDataSource('bar');
-        self::assertInstanceOf(AccountInterface::class, $customerDataSource->find("foo"));
+        self::assertInstanceOf(AccountInterface::class, self::$customerDataSource->find("foo"));
+    }
+
+    public function isAccountCompatibleDataProvider(): array
+    {
+        return [
+            [true, 'customers/123'],
+            [false, 'customer/123'],
+            [false, '123/customers'],
+            [false, '123/customer'],
+            [false, 'customer'],
+            [false, '123'],
+            [false, ''],
+        ];
+    }
+
+    /**
+     * @dataProvider isAccountCompatibleDataProvider
+     */
+    public function testIsAccountCompatible(bool $condition, string $accountId)
+    {
+        self::assertSame($condition, self::$customerDataSource->isAccountCompatible($accountId));
     }
 }
