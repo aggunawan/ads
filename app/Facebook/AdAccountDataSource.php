498a9f34f5f2e4676fd1c759787640b6383bd03e
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-17 16:08:59 +0700

    update constructor of AdAccountDataSource

--- app/Facebook/AdAccountDataSource.php
+++ app/Facebook/AdAccountDataSource.php
@@ -3,14 +3,27 @@
 namespace App\Facebook;
 
 use App\DataSource\Abstracts\BaseAccountDataSource;
+use App\Exceptions\UnresolvedAdAccount;
 use App\Objects\Account;
 use App\Objects\Contracts\AccountInterface;
+use ArgumentCountError;
+use FacebookAds\Api;
+use FacebookAds\Object\AdAccount;
+use FacebookAds\Object\Fields\AdAccountFields;
 use Illuminate\Support\Str;
+use InvalidArgumentException;
 use JetBrains\PhpStorm\Pure;
 
 class AdAccountDataSource extends BaseAccountDataSource
 {
-    #[Pure]
+    public function __construct(
+        private readonly ?Authentication $authentication = null
+    ) {
+        if (is_null(Api::instance()) and is_null($this->authentication)) throw new ArgumentCountError(
+            'Uninitialized Facebook API. Pass ' . Authentication::class . ' object to constructor of ' . __CLASS__
+        );
+    }
+
     public function find(string $accountId): ?AccountInterface
     {
         return new Account($accountId, 'Foo');
--- tests/Unit/Facebook/AdAccountDataSourceTest.php
+++ tests/Unit/Facebook/AdAccountDataSourceTest.php
@@ -2,24 +2,49 @@
 
 namespace Tests\Unit\Facebook;
 
+use App\Exceptions\UnresolvedAdAccount;
 use App\Facebook\AdAccountDataSource;
+use App\Facebook\Authentication;
 use App\Objects\Contracts\AccountInterface;
+use ArgumentCountError;
+use FacebookAds\Api;
+use FacebookAds\Http\Response;
 use JetBrains\PhpStorm\ArrayShape;
 use PHPUnit\Framework\TestCase;
+use ReflectionProperty;
 
+/**
+ * @testdox  "act_123" is compatible account id
+ * @testdox "act123" is incompatible account id
+ * @testdox "123_act" is incompatible account id
+ * @testdox "123" is incompatible account id
+ * @testdox "act" is incompatible account id
+ * @testdox "" is incompatible account id
+ *
+ * @testdox find return Account Object
+ * @testdox find thrown Unresolved Account
+ *
+ * @testdox construct without Facebook API Instance and don't pass Authentication
+ * @testdox construct with Facebook API Instance and don't pass Authentication
+ * @testdox construct without Facebook API Instance and pass Authentication
+ * @testdox construct with Facebook API Instance and pass Authentication
+ */
 class AdAccountDataSourceTest extends TestCase
 {
     static public ?AdAccountDataSource $adAccountDataSource;
 
-    public static function setUpBeforeClass(): void
+    public function setUp(): void
     {
-        self::$adAccountDataSource = new AdAccountDataSource('bar');
+        self::$adAccountDataSource = new AdAccountDataSource($this->createMock(Authentication::class));
         parent::setUpBeforeClass();
     }
 
-    public static function tearDownAfterClass(): void
+    public function tearDown(): void
     {
         self::$adAccountDataSource = null;
+        $ref = new ReflectionProperty(Api::class, 'instance');
+        /** @noinspection PhpRedundantOptionalArgumentInspection */
+        $ref->setValue(Api::instance(), null);
         parent::tearDownAfterClass();
     }
 
@@ -56,5 +81,36 @@ class AdAccountDataSourceTest extends TestCase
     public function testIsAccountCompatible(bool $condition, string $accountId)
     {
         self::assertSame($condition, self::$adAccountDataSource->isAccountCompatible($accountId));
+    }
+
+    public function testConstructWithoutApiInstanceAndParam()
+    {
+        $this->expectException(ArgumentCountError::class);
+        $this->expectExceptionMessage('Uninitialized Facebook API. Pass App\Facebook\Authentication object to constructor of App\Facebook\AdAccountDataSource');
+        new AdAccountDataSource();
+    }
+
+    public function testConstructWithApiButDoNotPassParam()
+    {
+        self::assertNull(Api::instance());
+        Api::setInstance($this->createMock(Api::class));
+        self::assertInstanceOf(AdAccountDataSource::class, new AdAccountDataSource());
+    }
+
+    public function testConstructWithoutApiButPassParam()
+    {
+        self::assertNull(Api::instance());
+        self::assertInstanceOf(AdAccountDataSource::class, new AdAccountDataSource(
+            $this->createMock(Authentication::class)
+        ));
+    }
+
+    public function testConstructWithApiAndPassParam()
+    {
+        self::assertNull(Api::instance());
+        Api::setInstance($this->createMock(Api::class));
+        self::assertInstanceOf(AdAccountDataSource::class, new AdAccountDataSource(
+            $this->createMock(Authentication::class)
+        ));
     }
 }
