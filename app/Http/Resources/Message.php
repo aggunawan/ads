862d1618401825ddea2fa9844f2143e4d6a1ab2a
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-18 16:28:36 +0700

    update Message schema

--- app/Http/Resources/Message.php
+++ app/Http/Resources/Message.php
@@ -14,6 +14,7 @@ use OpenApi\Attributes\Schema;
     properties: [
         new Property(
             property: "message",
+            description: "A Message",
             type: "string"
         )
     ]
